091c8813bdda5d3360e43193eed86ae96d421d4e
chunChu <doanthanhchung03nb@gmail.com>
2024-06-13 16:24:50 +0700

    feat: move pages to auth folder & add img for product

--- frontend/src/pages/auth/LoginPage.jsx
+++ frontend/src/pages/auth/LoginPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { FaGoogle } from "react-icons/fa";
-import banner from "../assets/banner.png";
+import banner from "../../assets/banner.png";
 
 const LoginPage = () => {
   return (
