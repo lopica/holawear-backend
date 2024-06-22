bf8709a621a1f8e64de5690679d994dcba0cc60a
chunChu <doanthanhchung03nb@gmail.com>
2024-06-22 22:24:03 +0700

    BE: add category model

--- backend/models/index.js
+++ backend/models/index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const User = require("./user.model");
 const Role = require("./role.model");
 const Product = require("./product.model");
+const Category = require("./category.model");
 
 mongoose.Promise = global.Promise;
 const db = {};
