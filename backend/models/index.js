c7dc1fd753a844b1e356b13db863bfc00b0f7a26
chunChu <doanthanhchung03nb@gmail.com>
2024-06-22 23:49:38 +0700

    test1

--- backend/models/index.js
+++ backend/models/index.js
@@ -2,8 +2,7 @@ const mongoose = require("mongoose");
 
 const User = require("./user.model");
 const Role = require("./role.model");
-const Product = require("./product.model");
-
+const Tag = require("./tag.model");
 mongoose.Promise = global.Promise;
 const db = {};
 db.user = User;
