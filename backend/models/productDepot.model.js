6ee849d68b90f033e5cb6cba85f371e84b67f057
chunChu <doanthanhchung03nb@gmail.com>
2024-07-02 18:23:56 +0700

    BE: add timest for product depot

--- backend/models/productDepot.model.js
+++ backend/models/productDepot.model.js
@@ -10,14 +10,19 @@ const stockSchema = new mongoose.Schema({
   details: [stockDetailSchema],
 });
 
-const productDepotSchema = new mongoose.Schema({
-  productId: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
-  importPrice: { type: Number, required: true },
-  stock: { type: Number, required: true },
-  stockDetails: [stockSchema],
-  importTotal: { type: Number, required: true },
-  createdAt: { type: Date, default: Date.now },
-});
+const productDepotSchema = new mongoose.Schema(
+  {
+    productId: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
+    importPrice: { type: Number, required: true },
+    stock: { type: Number, required: true },
+    stockDetails: [stockSchema],
+    importTotal: { type: Number, required: true },
+    createdAt: { type: Date, default: Date.now },
+  },
+  {
+    timestamps: true,
+  },
+);
 
 const ProductDepot = mongoose.model("ProductDepot", productDepotSchema);
 
