9769974e15f40958ea18340be28f9a52f4ffca16
chunChu <doanthanhchung03nb@gmail.com>
2024-07-08 17:43:03 +0700

    conf coming soon
    Co-Authored-By: Ha Gia Manh <144305528+HaGiaManhHE176443@users.noreply.github.com>
    Co-Authored-By: NguyenThanhTung <133035143+NguyenThanhTung1006@users.noreply.github.com>
    Co-Authored-By: Enric Van <122770211+enricVan@users.noreply.github.com>
    Co-Authored-By: DoDucThien-HE176216 <109223300+DoDucThien-He176216@users.noreply.github.com>

--- frontend/src/pages/error/ComingSoon.jsx
+++ frontend/src/pages/error/ComingSoon.jsx
@@ -8,14 +8,6 @@ const ComingSoon = () => {
           <div className="max-w-2xl w-full px-4">
             <h1 className="text-4xl font-bold text-center mb-8 text-gray-900 dark:text-white">Coming Soon!</h1>
             <p className="text-lg text-gray-600 dark:text-gray-300 text-center mb-12">This feature will be developed soon in the future!</p>
-            {/* <form className="flex flex-col md:flex-row justify-center items-center gap-4">
-              <input
-                className="w-full md:w-80  py-2 px-4 border text-gray-800 dark:text-white border-gray-200 dark:border-gray-600 bg-white dark:bg-gray-700"
-                type="email"
-                placeholder="Enter your email address"
-              />
-              <button className="bg-blue-600 hover:bg-blue-700 text-white py-2 px-4 border dark:bg-blue-800">Notify Me</button>
-            </form> */}
           </div>
         </div>
       </div>
