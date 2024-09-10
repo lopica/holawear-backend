0f58bb3c3512006b0ae36ebe6903fd5de871414b
dnthchung <doanthanhchung03nb@gmail.com>
2024-09-10 20:11:52 +0700

    modify product detail
    - đang dở tay đoạn xử lý chọn số lượng sản phẩm.

---
+++ mobile app/app/src/main/java/vn/edu/fpt/holawear/activities/product/ProductDetailActivity.java
@@ -0,0 +1,21 @@
+package vn.edu.fpt.holawear.activities.product;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import vn.edu.fpt.holawear.R;
+
+public class ProductDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_detail);
+
+    }
+}
