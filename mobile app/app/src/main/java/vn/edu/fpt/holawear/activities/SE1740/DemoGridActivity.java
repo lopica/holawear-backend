7f9459512b2386728f5f0233a3838f5b888043e2
dnthchung <doanthanhchung03nb@gmail.com>
2024-09-12 01:34:42 +0700

    init

---
+++ mobile app/app/src/main/java/vn/edu/fpt/holawear/activities/SE1740/DemoGridActivity.java
@@ -0,0 +1,21 @@
+package vn.edu.fpt.holawear.activities.SE1740;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import vn.edu.fpt.holawear.R;
+
+public class DemoGridActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo_grid);
+
+    }
+}
