4cdbd46157218fd7186e52c4357e6334e68a5e94
dnthchung <doanthanhchung03nb@gmail.com>
2024-09-20 01:38:23 +0700

    modify

--- backend/models/brand.model.js
+++ backend/models/brand.model.js
@@ -6,7 +6,7 @@ const brandSchema = new Schema(
     name: { type: String, required: true },
     description: { type: String },
     image: { type: String, default: "" }, // Đường dẫn đến hình ảnh của thương hiệu, mặc định là rỗng
-    status: { type: Boolean, default: true }
+    status: { type: Boolean, default: true },
   },
   { timestamps: true },
 );
